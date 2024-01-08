dc586ca059f712ab68d8a34412cc7b30f61509ef
Sidan <you@example.com>
2024-01-07 21:08:35 -0500

    update the log error

--- sdxl/sdxl.py
+++ sdxl/sdxl.py
@@ -10,8 +10,11 @@ pip install -q diffusers transformers accelerate omegaconf
 from diffusers import AutoPipelineForText2Image
 import torch
 
+#device = torch.device("cpu")
 pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
 pipeline_text2image = pipeline_text2image.to("cuda")
+#pipeline_text2image = pipeline_text2image.to("cpu")
+#device = torch.device("cpu")
 
 prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
 
