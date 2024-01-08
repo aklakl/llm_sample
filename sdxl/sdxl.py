e569317f5e442d1108af46c2f441c5a703ac2f9b
Sidan <you@example.com>
2024-01-07 23:02:03 -0500

    fix some bugs

--- sdxl/sdxl.py
+++ sdxl/sdxl.py
@@ -17,7 +17,7 @@ from diffusers.utils import load_image, make_image_grid
 
 import torch
 gpu = torch.cuda.is_available()
-opt_usage = f"{opt}{'GPU' if gpu else 'CPU'}usage"
+opt_usage = f"{'GPU' if gpu else 'CPU'}usage"
 print(opt_usage)
 
 def run_sdxl_turbo_pipeline_text2image():
@@ -49,17 +49,22 @@ def run_sdxl_turbo_pipeline_text2image():
 def run_sdxl_turbo_pipeline_image2image():
     try:
         # use from_pipe to avoid consuming additional memory when loading a checkpoint
+        pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
         if gpu==True:
             pipeline = AutoPipelineForImage2Image.from_pipe(pipeline_text2image).to("cuda")     
         else:
             pipeline = AutoPipelineForImage2Image.from_pipe(pipeline_text2image)
 
+        print("==========000==============")
         init_image = load_image("https://oss.feidee.net/oss//group_oss_trans3_f9651cb59a6ee921_1597X1276.jpg")
-        init_image = init_image.resize((1024, 1024))
+        init_image = init_image.resize((1597, 1276))
         prompt = "Enhance the cuteness of this portrait, please."
         image = pipeline(prompt, image=init_image, strength=0.5, guidance_scale=0.0, num_inference_steps=2).images[0]
+        print("==========1111==============")
         make_image_grid([init_image, image], rows=1, cols=2)
+        print("==========2222==============")
         res_image = image.save("./images/image2image.jpg") 
+        print("==========Completed run_sdxl_turbo_pipeline_image2image==============")
     except Exception as e:
         print(f"""run_sdxl_turbo_pipeline_image2image failed with Exception{e}. \n""")    
 
