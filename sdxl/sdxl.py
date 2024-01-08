35012da0fdd97f3200cf98e445cbef56af7085cb
Sidan <you@example.com>
2024-01-07 22:05:02 -0500

    add SDXL

--- sdxl/sdxl.py
+++ sdxl/sdxl.py
@@ -11,14 +11,23 @@ from diffusers import AutoPipelineForText2Image
 import torch
 
 #device = torch.device("cpu")
-pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16")
-pipeline_text2image = pipeline_text2image.to("cuda")
-#pipeline_text2image = pipeline_text2image.to("cpu")
-#device = torch.device("cpu")
+#This is for cuda GPU
+#pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo", torch_dtype=torch.float16, variant="fp16").to("cuda")
 
-prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+#This is for cpu
+pipeline_text2image = AutoPipelineForText2Image.from_pretrained("stabilityai/sdxl-turbo")
+
+
+
+#prompt = "A cinematic shot of a baby racoon wearing an intricate italian priest robe."
+prompt = "cute dogs"
+prompt = "cat wizard, gandalf, lord of the rings, detailed, fantasy, cute, adorable, Pixar, Disney, 8k, with happy birthday word"
+prompt = "cute cat Abyssinian, stand on the globe"
+
 
 image = pipeline_text2image(prompt=prompt, guidance_scale=0.0, num_inference_steps=1).images[0]
 image
-
+#print(image)
+# save a image using extension  refer:https://www.geeksforgeeks.org/python-pil-image-save-method/
+im1 = image.save("./images/Abyssinian stand on the globe.jpg") 
 print("========================")
