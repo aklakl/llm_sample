bb7ac920b9b3d7db7fbe24ae8555fe1edd6ae4a6
Ming li <4384886+aklakl@users.noreply.github.com>
2024-01-20 21:17:37 +0000

    add ngrok

--- ollamademo/run_ollama.sh
+++ ollamademo/run_ollama.sh
@@ -3,4 +3,9 @@ nohup ollama serve >>ollamaServices.log &
 
 #Docs=>https://developers.cloudflare.com/cloudflare-one/connections/connect-networks/do-more-with-tunnels/trycloudflare/
 #Downlaod => https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+#Please note that Quick Tunnels are meant to be ephemeral and should only be used for testing purposes. For production usage, we recommend creating Named Tunnels. (https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide/)
+#Expose my ollama service for outside temporary using cloudflared
 cloudflared tunnel --url localhost:11434/
+
+#Start ngrok for ssh
+ngrok tcp 22 > /dev/null &
