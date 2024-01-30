157b18a3032d90d8e3513db5f92b9077788a7747
Sidan Lu <sl6723@adroit-vis.princeton.edu>
2024-01-30 16:04:47 -0500

    add some configuration with litellm

--- ollamademo/run_ollama.sh
+++ ollamademo/run_ollama.sh
@@ -5,7 +5,7 @@ nohup ollama serve >>ollamaServices.log &
 #Downlaod => https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 #Please note that Quick Tunnels are meant to be ephemeral and should only be used for testing purposes. For production usage, we recommend creating Named Tunnels. (https://developers.cloudflare.com/cloudflare-one/connections/connect-apps/install-and-setup/tunnel-guide/)
 #Expose my ollama service for outside temporary using cloudflared
-cloudflared tunnel --url localhost:11434/
+cloudflared tunnel --url localhost:11434/ 
 
 #Start ngrok for ssh
 #Generate an SSH Key Pair on UNIX and UNIX-Like Systems=>https://docs.oracle.com/en/cloud/cloud-at-customer/occ-get-started/generate-ssh-key-pair.html#GUID-8B9E7FCB-CEA3-4FB3-BF1A-FD3406A2432F
